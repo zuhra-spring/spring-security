4e37d8b425829025c0d48b4f2b60deacb9ff5c8b
subairsuvt <subair.s@uvionicstech.com>
2019-12-17 09:25:36 +0530

    intialize

---
+++ src/test/java/com/zuhra/springsecurity/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zuhra.springsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
